0d342e4b66b86a0b005b3b8ab1b7f4597bf243a7
Tom Hooper <tomwbhooper@hotmail.com>
2021-09-02 11:06:40 +1200

    create stamps db functions

--- server/db/stamps.js
+++ server/db/stamps.js
@@ -1,8 +1,15 @@
 const connection = require('./connection')
 
 // get all users stamps but at the front need to filter out the shop and use length to count #of stamps?
-// sounds like a good idea - TH
-const getStampsbyId = (userId, db = connection) => {
+
+// sounds like a good idea. I have added another function that gets by shop id and refactored slightly, just incase we need it :). Also added other crud functions just incase we need them. I write databse functions the old way, just a habit because db's use older tech - TH
+
+function getStamps (db = connection) {
+  return db('stamps')
+    .select()
+}
+
+function getStampsByUserId (userId, db = connection) {
   return db('stamps')
     .select()
     .where('user_id', userId)
@@ -10,6 +17,36 @@ const getStampsbyId = (userId, db = connection) => {
     .join('shops', 'stamps.shop_id', 'shops.id')
 }
 
+function getStampsByShopId (shopId, db = connection) {
+  return db('stamps')
+    .select()
+    .where('shop_id', shopId)
+    .join('users', 'stamps.user_id', 'users.id')
+    .join('shops', 'stamps.shop_id', 'shops.id')
+}
+
+function addStamp (stamp, db = connection) {
+  return db('stamps')
+    .insert(stamp)
+}
+
+function editStamp (stamp, db = connection) {
+  return db('stamps')
+    .where('id', stamp.id)
+    .update(stamp)
+}
+
+function deleteStamp (id, db = connection) {
+  return db('stamps')
+    .where('id', id)
+    .del()
+}
+
 module.exports = {
-  getStampsbyId
+  getStamps,
+  getStampsByUserId,
+  getStampsByShopId,
+  addStamp,
+  editStamp,
+  deleteStamp
 }
