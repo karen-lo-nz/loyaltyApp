b2f41e8204e0c8d33afa1cc0e24f4af4d9d5a14a
Tom Hooper <tomwbhooper@hotmail.com>
2021-09-02 10:59:19 +1200

    create shops db functions

--- server/db/migrations/20210901164730_shops.js
+++ server/db/migrations/20210901164730_shops.js
@@ -1,13 +1,12 @@
-exports.up = function(knex) {
-    return knex.schema.createTable('shops',table => {
-        table.increments('id')
-        table.string('name')
-        table.string('address')
-        table.string('email')
-    })
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTable('shops')
-  };
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('shops', table => {
+    table.increments('id')
+    table.string('name')
+    table.string('address')
+    table.string('email')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('shops')
+}
--- server/db/stamps.js
+++ server/db/stamps.js
@@ -1,13 +1,13 @@
 const connection = require('./connection')
 
-
-//get all users stamps but at the front need to filter out the shop and use length to count #of stamps?
-const getStampsbyId = (userId,db = connection) => {
+// get all users stamps but at the front need to filter out the shop and use length to count #of stamps?
+// sounds like a good idea - TH
+const getStampsbyId = (userId, db = connection) => {
   return db('stamps')
-  .select()
-  .where('user_id', userId)
-  .join('users', 'stamps.user_id', 'users.id')
-  .join('shops', 'stamps.shop_id', 'shops.id')
+    .select()
+    .where('user_id', userId)
+    .join('users', 'stamps.user_id', 'users.id')
+    .join('shops', 'stamps.shop_id', 'shops.id')
 }
 
 module.exports = {
