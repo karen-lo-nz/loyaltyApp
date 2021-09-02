e2320f35fc503181ab10de808e5e74979bec176e
Tom Hooper <tomwbhooper@hotmail.com>
2021-09-03 10:19:36 +1200

    refactor stamps and stamps tests, joins fixed

--- server/db/stamps.test.js
+++ server/db/stamps.test.js
@@ -22,31 +22,52 @@ test('getStamps returns all stamps', () => {
       expect(stamps).toHaveLength(9)
       return null
     })
-    .catch(err => console.log(err))
 })
 
-// Not what I expected, I thought it should return the entries from the other tables as well? - Karen could you have a look? TH
-
-// should this function only have one join to the users table and the other function that gets by shop id have a join to the shops table?
+test('getOneStanp returns one stamp', () => {
+  return db.getOneStamp(1, testDb)
+    .then(stamp => {
+      expect(stamp.user_id).toEqual(1)
+      return null
+    })
+})
 
 test('getStampsByUserId gets all stamps that match user_id and return matching entries in  from user and shops table', () => {
   return db.getStampsByUserId(1, testDb)
-    .then(result => {
-      console.log(result)
-      expect(result).toHaveLength(3)
+    .then(stamps => {
+      expect(stamps[0]).toEqual({
+        id: 1,
+        created_at: 1092021,
+        user_id: 1,
+        shop_id: 1,
+        user_name: 'Joe',
+        shop_name: 'KFC',
+        user_email: 'joe@gmail.com',
+        user_phone: 123,
+        shop_address: 'Brooklyn',
+        shop_email: 'kfc@gmail.com'
+      })
       return null
     })
-    .catch(err => console.log(err))
 })
 
 test('getStampsByShopId gets all stamps that match shop_id and return mataching entries in from users and shops table', () => {
   return db.getStampsByShopId(1, testDb)
-    .then(result => {
-      console.log(result)
-      expect(result).toHaveLength(4)
+    .then(stamps => {
+      expect(stamps[0]).toEqual({
+        id: 1,
+        created_at: 1092021,
+        user_id: 1,
+        shop_id: 1,
+        user_name: 'Joe',
+        shop_name: 'KFC',
+        user_email: 'joe@gmail.com',
+        user_phone: 123,
+        shop_address: 'Brooklyn',
+        shop_email: 'kfc@gmail.com'
+      })
       return null
     })
-    .catch(err => console.log(err))
 })
 
 test('addStamp adds a stamp and returns the new stamp Id', () => {
@@ -72,7 +93,6 @@ test('editStamp edits a stamp and returns number of stamps edited', () => {
       expect(stampsEdited).toEqual(1)
       return null
     })
-    .catch(err => console.log(err))
 })
 
 test('deleteStamp deletes a stamp and returns number of stamps deleted', () => {
@@ -81,5 +101,4 @@ test('deleteStamp deletes a stamp and returns number of stamps deleted', () => {
       expect(stampsDeleted).toEqual(1)
       return null
     })
-    .catch(err => console.log(err))
 })
