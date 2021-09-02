422bbb607786a7c256266e04d3671ddfd8924515
Karen <lopk.karen@gmail.com>
2021-09-02 15:32:19 +1200

    add all routes to server file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 
 const stampsRoutes = require('./routes/stamps')
+const usersRoutes = require ('./routes/users')
+const shopsRoutes = require ('./routes/shops')
 
 const server = express()
 
@@ -9,5 +11,7 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/stamps', stampsRoutes)
+server.use('/api/v1/users', usersRoutes)
+server.use('/api/v1/shops', shopsRoutes)
 
 module.exports = server
