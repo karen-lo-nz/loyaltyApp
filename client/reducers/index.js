481d50c745b37a9515c8a8a67cca87e2b6b3d0ab
Tom Hooper <tomwbhooper@hotmail.com>
2021-09-06 14:42:51 +1200

    create users reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 
-import fruits from './fruits'
+import users from './users'
 
 export default combineReducers({
-  fruits
+  users
 })
---
+++ client/reducers/users.js
@@ -0,0 +1,27 @@
+import { SET_USERS, CREATE_USER, EDIT_USER, REMOVE_USER } from '../actions/users'
+
+const initialState = []
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USERS:
+      return action.users
+
+    case CREATE_USER:
+      return [...state, action.user]
+
+    case EDIT_USER:
+      return state.map(user => {
+        if (user.id === action.user.id) return action.user.id
+        else return user
+      })
+
+    case REMOVE_USER:
+      return state.filter(user => user.id !== action.id)
+
+    default:
+      return state
+  }
+}
+
+export default reducer
